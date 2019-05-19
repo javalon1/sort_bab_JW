8ca7d41ab9a5e32ffb214f75b0930a01f0259f84
Jacek.Woj <jacek.wojtuszek@gmail.com>
2019-05-19 12:23:03 +0100

    sortowanie_babelkowe_finish

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -6,12 +6,18 @@ import java.util.Random;
 public class App {
     public static int[] sort(int[] tab) {
         for (int j = 0; j < tab.length - 1; j++) {
-            for (int i = 0; i < tab.length - 1; i++) {
+
+            boolean flaga = false;
+            for (int i = 0; i < tab.length - 1 - j; i++) {
                 if (tab[i] > tab[i + 1]) {
                     int tmp = tab[i + 1];
                     tab[i + 1] = tab[i];
                     tab[i] = tmp;
+                    flaga = true;
                 }
+            }
+            if(!flaga){
+                break;
             }
         }
         System.out.println(Arrays.toString(tab));
@@ -22,7 +28,7 @@ public class App {
         Random random = new Random();
         int max = 5000;
         int min = -5000;
-        int[] tab = new int[20];
+        int[] tab = new int[10000];
         long startTime = System.currentTimeMillis();
         for (int i = 0; i < tab.length; i++) {
             int randomNumber = random.nextInt(max - min + 1) + min;
