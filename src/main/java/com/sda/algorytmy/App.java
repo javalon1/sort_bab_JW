3556a960b8d0b7f9aa240880e86170a11d76d85f
Jacek.Woj <jacek.wojtuszek@gmail.com>
2019-05-19 11:14:58 +0100

    bab_posortowane

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,13 +1,41 @@
 package com.sda.algorytmy;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.Arrays;
+import java.util.Random;
+
+public class App {
+    public static int[] sort(int[] tab) {
+        for (int j = 0; j < tab.length - 1; j++) {
+            for (int i = 0; i < tab.length - 1; i++) {
+                if (tab[i] > tab[i + 1]) {
+                    int tmp = tab[i + 1];
+                    tab[i + 1] = tab[i];
+                    tab[i] = tmp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(tab));
+        return tab;
+    }
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        int max = 5000;
+        int min = -5000;
+        int[] tab = new int[20];
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < tab.length; i++) {
+            int randomNumber = random.nextInt(max - min + 1) + min;
+            tab[i] = randomNumber;
+            System.out.println(tab[i]);
+        }
+        long endTime = System.currentTimeMillis();
+        long totalTime = endTime - startTime;
+        System.out.println("Czas wykonywania: " + totalTime);
+        startTime = System.currentTimeMillis();
+        sort(tab);
+        endTime = System.currentTimeMillis();
+        totalTime = endTime - startTime;
+        System.out.println("Czas sortowania: " + totalTime);
     }
 }
